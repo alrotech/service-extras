052ce0e8078d072e23c2d5f7236303d6e98eec4f
Ivan Klimchuk <klimchuk@1pt.com>
2016-05-10 15:25:48 +0300

    Init

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+require "../vendor/autoload.php";
+
+$config['displayErrorDetails'] = true;
+
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$container['logger'] = function ($c) {
+    $logger = new Monolog\Logger('my_logger');
+    $fileHandler = new Monolog\Handler\StreamHandler('../logs/app.log');
+    $logger->pushHandler($fileHandler);
+
+    return $logger;
+};
+
+$container['xml'] = function ($c) {
+    return new Sabre\Xml\Service();
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+// verify repository
+// todo: add checks of login and key
+$app->get('/verify', function (Request $request, Response $response) {
+    /** @var Response $response */
+    $response = $response->withStatus(200);
+    $response = $response->withHeader('Content-Type', 'application/xml');
+    $response->getBody()->write($this->xml->write('status', ['verified' => 1]));
+
+    return $response;
+});
+
+$app->run();
