ff381065f3cdb1855eb0c770fd5094a9c3c86ce8
Ivan Klimchuk <klimchuk@1pt.com>
2016-06-03 17:02:47 +0300

    Added deploy configuration

---
+++ deploy.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'recipe/composer.php';
+
+// Set configurations
+set('repository', 'git@github.com:Alroniks/aes-repository.git');
+set('shared_files', []);
+set('shared_dirs', [
+    'config'
+]);
+set('writable_dirs', []);
+
+
+server('production', 'api.aestore.by')
+    ->user('alroniks')
+    ->identityFile()
+    ->env('deploy_path', '/var/www/apiaestore')
+    ->stage('production');
+
+task('php:restart', function () {
+    run('sudo /usr/sbin/service php7.0-fpm restart');
+})->desc('Restart PHP-FPM service');
+
+after('success', 'php:restart');
+
+after('deploy:update_code', 'deploy:shared');
